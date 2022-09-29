ae25dae704f37f0d1375530ac41a9ddb232c4361
Baangkok Vanijyananda <iambaangkok@gmail.com>
2022-09-29 14:49:21 +0700

    Created: Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Navbar() {
+    
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                HELLO M
+            </header>
+        </div>
+    );
+}
+
+export default Navbar;
