7c6e10001b5425b074c412ab856c194c1564eb16
Baangkok Vanijyananda <iambaangkok@gmail.com>
2022-10-14 15:53:40 +0700

    Added: Responsive to current page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,29 @@ import '../css/Navbar.css'
 import Brand from './Brand';
 import Button from './Button';
 import SearchBar from './SearchBar';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 
-function Navbar(){
+function Navbar() {
+
+    const useMatchPath = (path:string) =>{
+        let location = useLocation();
+        var pagePath =  location.pathname;
+        return pagePath.startsWith(path);
+    }
+
+    const useMatchPathExact = (path:string) =>{
+        let location = useLocation();
+        var pagePath =  location.pathname;
+        return pagePath == (path);
+    }
+
+    useEffect(() => {
+
+    }, [])
 
 
-    return(
+    return (
         <div className={"Navbar"}>
             <div className={"BrandContainer"}>
                 <Brand></Brand>
@@ -18,26 +34,24 @@ function Navbar(){
             <div className="ButtonContainer">
                 <div className="Left">
                     <Link to="/">
-                        <Button text={"Home"} icon={""} buttonColor={"white"} textColor={"yellow"} ></Button>
+                        <Button text={"Home"} icon={""} buttonColor={"white"} textColor={useMatchPathExact("/") ? "yellow" : "black"} ></Button>
                     </Link>
                     <Link to="/products">
-                        <Button text={"Products"} icon={""} buttonColor={"white"} textColor={"black"} ></Button>
+                        <Button text={"Products"} icon={""} buttonColor={"white"} textColor={useMatchPath("/products") ? "yellow" : "black"} ></Button>
                     </Link>
                     <Link to="/orders">
-                        <Button text={"My Orders"} icon={""} buttonColor={"white"} textColor={"black"} ></Button>
+                        <Button text={"My Orders"} icon={""} buttonColor={"white"} textColor={useMatchPath("/orders") ? "yellow" : "black"} ></Button>
                     </Link>
 
                 </div>
                 <div className="Right">
                     <SearchBar></SearchBar>
                     <Link to="/login">
-                        <Button text={""} icon={"person"} buttonColor={"white"} textColor={"black"} ></Button>
+                        <Button text={""} icon={"person"} buttonColor={"white"} textColor={useMatchPath("/login") ? "yellow" : "black"} ></Button>
                     </Link>
                     <Link to="/cart">
-                        <Button text={""} icon={"shopping_cart_outline"} buttonColor={"white"} textColor={"black"} ></Button>
+                        <Button text={""} icon={"shopping_cart_outline"} buttonColor={"white"} textColor={useMatchPath("/cart") ? "yellow" : "black"} ></Button>
                     </Link>
-
-
                 </div>
             </div>
         </div>
