edfbc8dd5ca15cb8f06dcf264e740b78182f483a
winnapat <GGgun2013@gmail.com>
2022-10-17 14:50:12 +0700

    add variable in login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,8 +3,28 @@ import '../css/Navbar.css'
 import '../css/Login.css'
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
+const url = "http://127.0.0.1:8000/login";
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [check,setcheck] = useState(true);
+
+    const postData = async () => {
+        try{
+            const resp = await axios.post(url,{
+                email:email,
+                password:password
+            });
+            console.log(resp)
+        }catch(e){
+            console.log(e)
+        }
+    }
+
     return (
         <div className={"Login"}>
             <div className={"LoginContainer"}>
@@ -14,14 +34,29 @@ function Login() {
                             Login
                         </div>
                     </div>
-                    <InputField textL="email" textR="" />
-                    <InputField textL="password" textR="forgot password?" />
+                    <div className="InputField">
+                        <div className='InputField'>
+                            <InputField textL="email" textR="" />
+                            <input className='Field' onChange={(e) => setEmail(e.target.value)}></input>
+                        </div>
+                        <div className='InputField'>
+                            <InputField textL="password" textR="forgot password?" />
+                            <input className='PassWord' onChange={(e) => setPassword(e.target.value)}></input>
+                        </div>
+                    </div>
                     <div className="SignInButtonContainer">
-                        <Button text={"Sign in"} icon={""} buttonColor={"black"} textColor={"white"} func={()=>{}}></Button>
+                        <Button text={"Sign in"} icon={""} buttonColor={"black"} textColor={"white"} func={() => { 
+                            {if(check){
+                                postData()
+                                window.location.href = "/products"
+                            }else{
+                                alert("Invalid email or password")
+                            }}
+                        }}></Button>
                     </div>
                     <div className="CreateAccContainer">
                         <Link to="/signup">
-                            <Button text={"Create an Account"} icon={""} buttonColor={"whiteBorder"} textColor={"gray"} func={()=>{}}></Button>
+                            <Button text={"Create an Account"} icon={""} buttonColor={"whiteBorder"} textColor={"gray"} func={() => { }}></Button>
                         </Link>
                     </div>
                 </div>
