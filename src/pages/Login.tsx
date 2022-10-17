2378d02ee7d211237e852462c46aaebf0edfdcf6
winnapat <GGgun2013@gmail.com>
2022-10-17 22:46:53 +0700

    add localStorange get and post in Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,15 +3,24 @@ import '../css/Navbar.css'
 import '../css/Login.css'
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const url = "http://127.0.0.1:8000/login";
+const geturl = "";
 
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [check,setcheck] = useState(true);
+    const data = localStorage.getItem("Token")
+    let pdata;
+    if(data){
+        pdata = JSON.parse(data)
+    }else{
+        pdata = [];
+    }
+    const [token,setToken] = useState(pdata);
 
     const postData = async () => {
         try{
@@ -20,10 +29,25 @@ function Login() {
                 password:password
             });
             console.log(resp)
+            
         }catch(e){
             console.log(e)
         }
     }
+
+    const fetchData = async () => {
+        try{
+            const resp1 = await axios.get(geturl);
+            const data = await resp1.data;
+            setToken(data);
+        }catch(e){
+            console.log(e)
+        }
+    }
+
+    useEffect(() =>{
+        localStorage.setItem('Token',JSON.stringify(token))
+    },[setToken,token])
 
     return (
         <div className={"Login"}>
@@ -46,8 +70,10 @@ function Login() {
                     </div>
                     <div className="SignInButtonContainer">
                         <Button text={"Sign in"} icon={""} buttonColor={"black"} textColor={"white"} func={() => { 
-                            {if(check){
-                                postData()
+                            {if(check && email.length !== 0 && password.length !== 0){
+                                setToken("111")
+                                // postData()
+                                // fetchData()
                                 window.location.href = "/products"
                             }else{
                                 alert("Invalid email or password")
