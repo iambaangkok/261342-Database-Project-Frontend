44f6363acd2a6ef4d3c26c7d3be8a70fbe79e0a9
winnapat <GGgun2013@gmail.com>
2022-10-28 14:13:14 +0700

    fix login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,95 +8,129 @@ import axios from "axios";
 
 const url = "http://127.0.0.1:8000/api/login";
 const geturl = "http://127.0.0.1:8000/api/login";
+const logouturl = "http://127.0.0.1:8000/api/logout";
 
 function Login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [check, setcheck] = useState(true);
-    const data = localStorage.getItem("Token")
-    let pdata;
-    let headers = new Headers();
+    let pdata: string| null;
 
-    headers.append('Content-Type', 'application/json');
-    headers.append('Accept', 'application/json');
 
-    headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
-    headers.append('Access-Control-Allow-Credentials', 'true');
 
-    if (data) {
-        pdata = JSON.parse(data)
-    } else {
-        pdata = [];
-    }
-    const [token, setToken] = useState(pdata);
+    const [token, setToken] = useState<string| null>(localStorage.getItem("Token"));
 
     const postData = async () => {
-        try {
-            const resp = await axios.post(url, {
-                username: username,
-                password: password
-            });
-            console.log(resp)
+        // try {
+        //     const resp = await axios.post(url, {
+        //         username: username,
+        //         password: password
+        //     });
+        //     console.log(resp)
+        // } catch (e) {
+        //     console.log(e)
+        //     alert("post Data fail")
+        // }
+        
 
-        } catch (e) {
-            console.log(e)
-            alert("post Data fail")
-        }
+    }
+
+    const postLogout = async () => {
+        // try {
+        //     const resp = await axios.post(logouturl, {
+        //         token:token
+        //     });
+        //     console.log(resp)
+        //     setToken(null)
+        // } catch (e) {
+        //     console.log(e)
+        //     alert("Logout fail")
+        // }
+        setToken(null)
+        localStorage.removeItem('Token')
     }
 
     const fetchData = async () => {
-        try {
-            const resp1 = await axios.get(geturl);
-            const data = await resp1.data;
-            setToken(data);
-            window.location.href = "/products"
-        } catch (e) {
-            console.log(e)
-            alert("get Token fail")
-        }
+        // try {
+        //     const resp1 = await axios.get(geturl);
+        //     const data = await resp1.data;
+        //     setToken(data);
+        //     window.location.href = "/products"
+        // } catch (e) {
+        //     console.log(e)
+        //     alert("get Token fail")
+        // }
+
+        var data = "1111"
+        setToken(data)
+        localStorage.setItem('Token', JSON.stringify(data))
     }
 
+
     useEffect(() => {
-        localStorage.setItem('Token', JSON.stringify(token))
-    }, [setToken, token])
+    }, [setToken,token])
+
 
     return (
         <div className={"Login"}>
             <div className={"LoginContainer"}>
-                <div className="Body">
-                    <div className="HeaderContainer">
-                        <div className="Text">
-                            Login
+                {token === null ?
+                    <div className="Body">
+                        <div className="HeaderContainer">
+                            <div className="Text">
+                                Login
+                            </div>
                         </div>
-                    </div>
-                    <div className="InputField">
-                        <div className='InputField'>
-                            <InputField textL="email" textR="" />
-                            <input className='Field' onChange={(e) => setUsername(e.target.value)}></input>
+                        <div className="InputField">
+                            <div className='InputField'>
+                                <InputField textL="email" textR="" />
+                                <input className='Field' onChange={(e) => setUsername(e.target.value)}></input>
+                            </div>
+                            <div className='InputField'>
+                                <InputField textL="password" textR="forgot password?" />
+                                <input className='PassWord' onChange={(e) => setPassword(e.target.value)}></input>
+                            </div>
                         </div>
-                        <div className='InputField'>
-                            <InputField textL="password" textR="forgot password?" />
-                            <input className='PassWord' onChange={(e) => setPassword(e.target.value)}></input>
-                        </div>
-                    </div>
-                    <div className="SignInButtonContainer">
-                        <Button text={"Sign in"} icon={""} buttonColor={"black"} textColor={"white"} func={() => {
-                            {
-                                if (check && username.length !== 0 && password.length !== 0) {
-                                    postData()
-                                    // fetchData()  
-                                } else {
-                                    alert("Invalid email or password")
+                        <div className="SignInButtonContainer">
+                            <Button text={"Sign in"} icon={""} buttonColor={"black"} textColor={"white"} func={() => {
+                                {
+                                    // if (check && username.length !== 0 && password.length !== 0) {
+                                        postData()
+                                        fetchData()  
+                                    // } else {
+                                        // alert("Invalid email or password")
+                                    // }
                                 }
-                            }
-                        }}></Button>
+                            }}></Button>
+                        </div>
+                        <div className="CreateAccContainer">
+                            <Link to="/signup" style={{ textDecoration: 'none' }}>
+                                <Button text={"Create an Account"} icon={""} buttonColor={"whiteBorder"} textColor={"gray"} func={() => { }}></Button>
+                            </Link>
+                        </div>
                     </div>
-                    <div className="CreateAccContainer">
-                        <Link to="/signup" style={{ textDecoration: 'none' }}>
-                            <Button text={"Create an Account"} icon={""} buttonColor={"whiteBorder"} textColor={"gray"} func={() => { }}></Button>
-                        </Link>
+                    : 
+                    <div className="Body">
+                        <div className="HeaderContainer">
+                            <div className="Text">
+                                Welcome
+                            </div>
+                        </div>
+                        <div className="SubHeaderContainer">
+                            <div className="Text">
+                                Username: sssssss
+                            </div>
+                            <div className="Text">
+                                Email: ssssssss@gmail.com
+                            </div>
+                        </div>
+                        <div className="CreateAccContainer">
+                            <Button text={"Logout"} icon={""} buttonColor={"black"} textColor={"white"} func={() => { 
+                                postLogout()
+                            }}></Button>
+                        </div>
                     </div>
-                </div>
+                    }
             </div>
         </div>
     );
