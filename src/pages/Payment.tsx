373a4af5be1bf0c3c4bfa74e1cd58c5c966ff5a6
winnapat <GGgun2013@gmail.com>
2022-11-06 21:39:30 +0700

    debug total sending is zero

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -17,63 +17,74 @@ type CartProductCardsProps = {
 }
 
 function Payment() {
-    var cartUrl = "http://127.0.0.1:8000/api/showcart"
-    var paymentUrl = "http://127.0.0.1:8000/api/payment"
+
+
 
     const [productCart, setCart] = useState([
         { productCode: "A", productName: "A", productLine: "LINE", productScale: "SCALE", productVendor: "VENDOR", productDescription: "DESC", quantity: 0, MSRP: 0 },
     ])
 
-    const [Subtotal, setSubTotal] = useState(0)
     const [checkNumber, setCheck] = useState("")
-    let total = 0
+    var total = 0
 
     const [isOpen, setIsOpen] = useState(false)
 
     const [alertText, setAlert] = useState("")
+
+    const [popUpH,setPopupHead] = useState("")
 
     const togglePopup = () => {
         setIsOpen(!isOpen)
     }
 
     const postToken = async () => {
+        var cartUrl = "http://127.0.0.1:8000/api/showcart"
         const resp = await axios.post(cartUrl, {
             remember_token: JSON.parse(localStorage.getItem("Token")!),
         }).then((resp) => {
             let data = resp.data
             console.log(data)
             setCart(data)
-        }).catch((error)=>{
+        }).catch((error) => {
             setAlert(error.response.data.message)
+            setPopupHead("Payment Fail")
             togglePopup()
             console.log(error)
         })
     }
 
     const postData = async () => {
+        var paymentUrl = "http://127.0.0.1:8000/api/payment"
         const resp = await axios.post(paymentUrl, {
             remember_token: JSON.parse(localStorage.getItem("Token")!),
-            totalAmount: Subtotal,
+            totalAmount: parseFloat(Number(total).toFixed(2)),
             checkNumber: checkNumber,
-        }).catch((error)=>{
-            setAlert(error.response.data.message)
+        }).then((resp) => {
+            console.log(resp)
+            setAlert("")
+            setPopupHead(resp.data)
             togglePopup()
-            console.log(error)
+            // window.location.href = "http://127.0.0.1:3000/login"
         })
+            .catch((error) => {
+                setAlert(error.response.data)
+                setPopupHead("Payment Fail")
+                togglePopup()
+                console.log(error)
+            })
     }
 
     useEffect(() => {
-        postToken()
+        postToken();
+           
     }, [])
 
 
-    useEffect(() => {
-        setSubTotal(parseFloat(Number(total).toFixed(2)))
-    }, [total])
 
     useEffect(() => {
-
     }, [productCart])
+
+
 
     return (
         <div className="PaymentContainer">
@@ -81,7 +92,7 @@ function Payment() {
                 <div className="HeadContainer">
                     <div className='Text'>Payment</div>
                 </div>
-                {isOpen && <PopUp handleClose={togglePopup} headText="Payment Fail" contentText={alertText}></PopUp>}
+                {isOpen && <PopUp handleClose={togglePopup} headText={popUpH} contentText={alertText}></PopUp>}
                 <div className='PaymentItem'>
                     <div className="ColumNames">
                         <div className='Left'>
@@ -115,13 +126,14 @@ function Payment() {
                         return <CartProductCards key={index} refreshFunction={() => postToken()} name={x.productName} vendor={x.productLine} scale={x.productScale} quantity={x.quantity} price={x.MSRP} total={sumTotal} productCode={x.productCode} remove={false} showSelectQuantity={false}></CartProductCards>
                     })}
 
+
                     <div className='SubTotal'>
                         <div className='Top'>
                             <div className='SubTotalText'>
                                 SubTotal:
                             </div>
                             <div className="SubTotalNumber">
-                                $ {parseFloat(Number(total).toFixed(2))}
+                                $ {total}
                             </div>
                         </div>
                         <div className='Mid'>
@@ -135,11 +147,11 @@ function Payment() {
                         <div className='Bottom'>
                             <div className='Frame'>
                                 <Button text={"Confirm Payment"} icon={""} buttonColor={"yellow"} textColor={"black"} func={() => {
-                                    if(checkNumber.length === 8){
-                                        postData()
-                                    }else{
-                                        setAlert("Check number must have 8 characters")
+                                    if (checkNumber.length > 8) {
+                                        setAlert("Check number must have at most 8 characters")
                                         togglePopup()
+                                    } else {
+                                        postData()
                                     }
                                 }}></Button>
                             </div>
